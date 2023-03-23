aae38f1db9f446b369ade30ff79c6fd8a77af04c
ayoubelk <ayoub1.elk@gmail.com>
2023-03-23 16:10:42 -0700

    task 2 & 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * prints the multiplication
+ * 
+ * of int a and int b
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * prints digits from 0 to 9
+ *
+ * using putchar
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+	
+	do {
+		_putchar(num + 48);
+			num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
