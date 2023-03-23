0dd838b8d848de835ab7f2e224a9c3b9034c9a34
ayoubelk <ayoub1.elk@gmail.com>
2023-03-23 16:17:14 -0700

    rectif task 2 n 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,13 @@
 /**
- * prints the multiplication
- * 
- * of int a and int b
- */
+ * mul - multiplies two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return results
+*/
 
 int mul(int a, int b)
-
 {
 	return (a * b);
 }
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include"main.h"
 
 /**
- * prints digits from 0 to 9
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
  *
- * using putchar
- */
+ * Return: Always 0 (Success)
+*/
 
 void print_numbers(void)
 {
 	int num = 0;
-	
+
 	do {
 		_putchar(num + 48);
-			num++;
+		num++;
 	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
