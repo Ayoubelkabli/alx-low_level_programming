cb2735ffafc2393b4aec18eb10a68b3b39c9369e
ayoubelk <ayoub1.elk@gmail.com>
2023-04-05 15:45:15 -0700

    corrected betty style on task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,13 @@
  * Returns (0)
  */
 
-void _puts_recursion(char *s) {
-	if (*s != '\0'){
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
 		_putchar(*s);
-		_puts_recursion(s+1);
-}
+		_puts_recursion(s+ 1);
+	}
 	else
 		_putchar('\n');
-		}
+}
